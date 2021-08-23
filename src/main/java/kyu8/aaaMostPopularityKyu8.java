2bdc788e725ec75ff55b7052c9d19f6cebe0d439
Kreout <ia@krot.name>
2021-08-24 00:10:36 +0300

    aaaMostPopularityKyu8

---
+++ src/main/java/kyu8/aaaMostPopularityKyu8.java
@@ -0,0 +1,30 @@
+package kyu8;
+
+public class aaaMostPopularityKyu8 {
+    public static void main(String[] args) {
+    }
+
+    public static String boolToWord(boolean b)
+    {
+        return b ? "Yes" : "No";
+    }
+    public static String reversedStrings(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static int summation(int n) {
+        if (n <= 0){
+            return 0;
+        }
+        int x = n;
+        for (int i = 1; i < x; i++) {
+            n += i;
+        }
+        return n;
+    }
+
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,4 @@ public class test {
     public static void main(String[] args) {
 
     }
-
 }
