01bc81838cf5e7832fdeafa43fa6440bfcfe845d
Kreout <ia@krot.name>
2021-08-30 22:13:52 +0300

    HumanReadableTime

--- src/main/java/kyu5/HumanReadableTime.java
+++ src/main/java/kyu5/HumanReadableTime.java
@@ -18,8 +18,31 @@ You can find some examples in the test fixtures.
         System.out.println(HumanReadableTime.makeReadable(5));
         System.out.println(HumanReadableTime.makeReadable(0));
     }
+
     public static String makeReadable(int seconds) {
-        // Do something
-        return Integer.toString(seconds);
+        if (seconds > 359999) {
+            return "error: very long";
+        }
+        int HH = 0;
+        int MM = 0;
+        int SS = 0;
+
+        final int KOEF_MM = 60;
+        final int KOEF_HH = 60 * 60;
+
+        HH = seconds / KOEF_HH;
+        seconds = seconds % KOEF_HH;
+        MM = seconds / KOEF_MM;
+        SS = seconds % KOEF_MM;
+
+        return addZero(HH) + ":" + addZero(MM) + ":" + addZero(SS);
+    }
+
+    public static String addZero(int i) {
+        if (i > 9 || i < 0) {
+            return "" + i;
+        } else {
+            return "0" + i;
+        }
     }
 }
