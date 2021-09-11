c3cc51f348802b847b8f4ee2f805939babc6b551
Овчаренко Андрей Александрович <ia@krot.name>
2021-09-11 14:34:43 +0300

    Create ReverseInt.java

---
+++ src/main/java/leetcode/ReverseInt.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class ReverseInt {
+
+    public static void main(String[] args) {
+        ReverseInt reverseInt = new ReverseInt();
+        System.out.println(reverseInt.reverse(123));
+        System.out.println(reverseInt.reverse(-123));
+        System.out.println(reverseInt.reverse(120));
+        System.out.println(reverseInt.reverse(0));
+    }
+
+    public int reverse(int x) {
+        if (x >= 0 ){
+            return Integer.parseInt(new StringBuilder(String.valueOf(x)).reverse().toString());
+        }
+        else {
+            return Integer.parseInt(new StringBuilder(String.valueOf(x).substring(1)).reverse().toString())*-1;
+        }
+    }
+
+}
