e47acfc32fae34b48462a1c98e9718f17f9750fc
Овчаренко Андрей <ia@krot.name>
2021-09-20 12:47:04 +0300

    Create NbYear.java

---
+++ src/main/java/kyu7/NbYear.java
@@ -0,0 +1,17 @@
+package kyu7;
+
+public class NbYear {
+    //7 https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/java
+    public static void main(String[] args) {
+        System.out.println(nbYear(1500, 5, 100, 5000)); //15
+    }
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int year = 0;
+        while (p0 < p){
+            p0 = (int) (p0+(p0*(percent/100)+aug));
+            year++;
+        }
+        return year;
+    }
+
+}
