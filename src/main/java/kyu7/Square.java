8bbfb51b9fe497bfc4ecb8e2dffe24a9e5458476
Овчаренко Андрей <ia@krot.name>
2021-09-20 12:08:55 +0300

    Create Square.java

---
+++ src/main/java/kyu7/Square.java
@@ -0,0 +1,13 @@
+package kyu7;
+
+public class Square {
+    //7 https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/java
+    public static void main(String[] args) {
+        System.out.println(isSquare(4));
+        System.out.println(isSquare(3));
+
+    }
+    public static boolean isSquare(int n) {
+        return Math.sqrt(n)%1 == 0;
+    }
+}
