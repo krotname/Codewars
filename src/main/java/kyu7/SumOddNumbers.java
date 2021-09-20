aceeab66c8d5051b22788d91f94bdf2aa555d436
Овчаренко Андрей <ia@krot.name>
2021-09-20 18:00:20 +0300

    Create SumOddNumbers.java

---
+++ src/main/java/kyu7/SumOddNumbers.java
@@ -0,0 +1,15 @@
+package kyu7;
+
+public class SumOddNumbers {
+    //7 https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/java
+    public static void main(String[] args) {
+        System.out.println(rowSumOddNumbers(2));
+        System.out.println(rowSumOddNumbers(42));
+    }
+    public static int rowSumOddNumbers(int n) {
+        //todo подобрать формулу
+/*        n=n*n+(n-1);
+        return (n*(n+1))/4;*/
+        return 0;
+    }
+}
