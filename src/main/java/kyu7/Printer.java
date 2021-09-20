ae3b936c624831af2e71b110ba6d5ed51ad7d0f9
Овчаренко Андрей <ia@krot.name>
2021-09-20 15:52:44 +0300

    Create Printer.java

---
+++ src/main/java/kyu7/Printer.java
@@ -0,0 +1,19 @@
+package kyu7;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Printer {
+    //7 https://www.codewars.com/kata/56541980fa08ab47a0000040/train/java
+    public static void main(String[] args) {
+        System.out.println(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")); // 3/56
+    }
+
+    public static String printerError(String s) {
+        Matcher matcher = Pattern.compile("[n-z]").matcher(s);
+        int error = 0;
+        while (matcher.find()){error++;}
+        return error + "/" + s.length();
+    }
+
+}
