7d58998d87191a135445d78a2237a9d25c2771c3
Овчаренко Андрей <ia@krot.name>
2021-09-20 12:16:17 +0300

    Create JadenCase.java

---
+++ src/main/java/kyu7/JadenCase.java
@@ -0,0 +1,17 @@
+package kyu7;
+
+public class JadenCase {
+    //7 https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/java
+
+    public static void main(String[] args) {
+        System.out.println(toJadenCase("most trees are blue"));
+    }
+    public static String toJadenCase(String phrase) {
+        if (phrase == null || phrase.length() == 0) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : phrase.split("\\s")) {
+            stringBuilder.append(s.substring(0,1).toUpperCase()).append(s.substring(1)).append(" ");
+        }
+        return stringBuilder.toString().trim();
+    }
+}
