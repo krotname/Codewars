129217ae4ad6a135157167b3ebeca95110d8de45
Овчаренко Андрей <ia@krot.name>
2021-09-20 15:41:53 +0300

    Update ValidatePin.java

--- src/main/java/kyu7/ValidatePin.java
+++ src/main/java/kyu7/ValidatePin.java
@@ -1,6 +1,5 @@
 package kyu7;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidatePin {
@@ -9,13 +8,10 @@ public class ValidatePin {
     public static void main(String[] args) {
         System.out.println(validatePin("1111"));
         System.out.println(validatePin("11111"));
+        System.out.println(validatePin("111111"));
     }
 
     public static boolean validatePin(String pin) {
-        //todo tests
-        final String regex = "\\d\\d\\d\\d";
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(pin);
-        return matcher.matches();
+        return Pattern.compile("[0-9]{4}|[0-9]{6}").matcher(pin).matches();
         }
 }
