42858d841ffd0063f9b4e59c6fd2d1090dc66106
Овчаренко Андрей <ia@krot.name>
2021-09-20 11:59:56 +0300

    Create Isogram.java

---
+++ src/main/java/kyu7/Isogram.java
@@ -0,0 +1,21 @@
+package kyu7;
+
+import java.util.HashSet;
+
+public class Isogram {
+    //7 https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/java
+    public static void main(String[] args) {
+        System.out.println(isIsogram("isIsogram")); //false
+        System.out.println(isIsogram("Dermatoglyphics")); //true
+
+    }
+
+    public static boolean isIsogram(String str) {
+        HashSet<Character> characters = new HashSet<>();
+        for (char c : str.toLowerCase().toCharArray()) {
+            if (characters.contains(c)) return false;
+            characters.add(c);
+        }
+        return true;
+    }
+}
