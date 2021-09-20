766a78b129ce61ee46b728114c8175ea9c2c2c01
Овчаренко Андрей <ia@krot.name>
2021-09-20 12:53:05 +0300

    Create TwoToOne.java

---
+++ src/main/java/kyu7/TwoToOne.java
@@ -0,0 +1,25 @@
+package kyu7;
+
+import java.util.TreeSet;
+
+public class TwoToOne {
+    //7 https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/java
+    public static void main(String[] args) {
+        System.out.println(longest("aretheyhere", "yestheyarehere")); //aehrsty
+    }
+
+    public static String longest(String s1, String s2) {
+        TreeSet<Character> characters = new TreeSet<>();
+        for (char c : s1.toCharArray()) {
+            characters.add(c);
+        }
+        for (char c : s2.toCharArray()) {
+            characters.add(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character character : characters) {
+            sb.append(character);
+        }
+        return sb.toString();
+    }
+}
