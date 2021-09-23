abd2ec6eac7268e6d5d719ea82ca1d08389e3a3a
Овчаренко Андрей <ia@krot.name>
2021-09-23 11:03:39 +0300

    decodeBits(String bits) passed

--- src/main/java/kyu4/MorseCodeDecoder.java
+++ src/main/java/kyu4/MorseCodeDecoder.java
@@ -1,4 +1,4 @@
-package kyu6;
+package kyu4;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -6,8 +6,9 @@ import java.util.Map;
 
 public class MorseCodeDecoder {
 
-    //7 https://www.codewars.com/kata/54b724efac3d5402db00065e
-    
+    //6 https://www.codewars.com/kata/54b724efac3d5402db00065e
+    //4 https://www.codewars.com/kata/54b72c16cd7f5154e9000457/train/java
+
     private static LinkedHashMap<String, String> alphabetToMorse = new LinkedHashMap<>();
 
     static Map<String, String> morseMap = new HashMap<>() {{
@@ -112,13 +113,17 @@ public class MorseCodeDecoder {
     }
 
     public static void main(String[] args) {
-        System.out.println(decode(".... . -.--   .--- ..- -.. ."));
-        System.out.println(decode("...-- -..."));
+        System.out.println(decodeMorse(".... . -.--   .--- ..- -.. ."));
+        System.out.println(decodeMorse(".... . -.--   .--- ..- -.. ."));
         System.out.println(encode("SOS THE QUICK BROWN FO JUMPS OVER THE LAZY DOG"));
-        System.out.println(decode("... --- ...     - .... .     --.- ..- .. -.-. -.-     -... .-. --- .-- -.     ..-. ---     .--- ..- -- .--. ...     --- ...- . .-.     - .... .     .-.. .- --.. -.--     -.. --- --."));
+        System.out.println(decodeMorse("... --- ...     - .... .     --.- ..- .. -.-. -.-     -... .-. --- .-- -.     ..-. ---     .--- ..- -- .--. ...     --- ...- . .-.     - .... .     .-.. .- --.. -.--     -.. --- --."));
+        System.out.println(decodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"));
+        System.out.println(decodeMorse(decodeBits("11011")));
+        System.out.println(decodeBits("111111011111111"));
+        System.out.println(decodeMorse(decodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")));
     }
 
-    public static String decode(String morseCode) {
+    public static String decodeMorse(String morseCode) {
         String[] split = morseCode.split("\\s");
         StringBuilder result = new StringBuilder();
         for (String s : split
@@ -141,4 +146,71 @@ public class MorseCodeDecoder {
         }
         return result.toString().toUpperCase().trim();
     }
+
+    public static String decodeBits(String bits) {
+        bits = trimStartZeros(bits);
+        int digits = checkDigits(bits);
+        char[] chars = bits.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < bits.length(); i += digits) {
+            stringBuilder.append(chars[i]);
+        }
+        stringBuilder.append("0000000");
+        StringBuilder result = new StringBuilder();
+        char[] chars1digits = stringBuilder.toString().toCharArray();
+        for (int i = 0; i < chars1digits.length - 6; i++) {
+            if (chars1digits[i] == 49) { // ASCI 1
+                if (chars1digits[i + 1] == 49 && chars1digits[i + 2] == 49) {
+                    result.append("-");
+                    i += 2;
+                } else {
+                    result.append(".");
+                }
+            }
+            if (chars1digits[i] == 48) { // ASCI 0
+                if (chars1digits[i + 1] == 48 && chars1digits[i + 2] == 48 && chars1digits[i + 3] == 48 && chars1digits[i + 4] == 48 && chars1digits[i + 5] == 48 && chars1digits[i + 6] == 48) {
+                    result.append("   ");
+                    i += 6;
+                } else if (chars1digits[i + 1] == 48 && chars1digits[i + 2] == 48) {
+                    result.append(" ");
+                }
+            }
+        }
+        return result.toString().trim();
+    }
+
+    public static String trimStartZeros(String bits) {
+        while (bits.startsWith("0")) {
+            bits = bits.substring(1);
+        }
+        return bits;
+    }
+
+    public static int checkDigits(String bits) {
+        int minCount1 = Integer.MAX_VALUE;
+        int minCount0 = Integer.MAX_VALUE;
+        int count1 = 0;
+        int count0 = 0;
+        char[] chars = bits.toCharArray();
+        for (int i = 0; i < bits.length(); i++) {
+            if (chars[i] == 49) { //ASCI = 1
+                count1++;
+            } else {
+                if (count1 > 0) {
+                    minCount1 = Math.min(count1, minCount1);
+                }
+                count1 = 0;
+            }
+            if (chars[i] == 48) { //ASCI = 0
+                count0++;
+            } else {
+                if (count0 > 0) {
+                    minCount0 = Math.min(count0, minCount0);
+                }
+                count0 = 0;
+            }
+        }
+        return Math.min(minCount0, minCount1);
+    }
+
 }
