f8f34c1779411ada686f06dd1a90b227041786b2
Овчаренко Андрей <ia@krot.name>
2021-09-28 16:04:50 +0300

    Create LineNumbering.java

---
+++ src/main/java/kyu7/LineNumbering.java
@@ -0,0 +1,21 @@
+package kyu7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LineNumbering {
+    //7 https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/java
+
+    public static void main(String[] args) {
+        System.out.println(number(Arrays.asList("a", "b", "c")));
+    }
+
+    public static List<String> number(List<String> lines) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            arrayList.add(i + 1 + ": " + lines.get(i));
+        }
+        return arrayList;
+    }
+}
