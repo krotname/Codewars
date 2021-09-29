79f3fda4f31509fead400fef22fe073895bba6e9
Овчаренко Андрей <ia@krot.name>
2021-09-29 17:42:13 +0300

    Create IsAnagram.java

---
+++ src/main/java/kyu7/IsAnagram.java
@@ -0,0 +1,18 @@
+package kyu7;
+
+import java.util.Arrays;
+
+public class IsAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("foefet", "toffee"));
+        System.out.println(isAnagram("Buckethead", "DeathCubeK"));
+    }
+
+    public static boolean isAnagram(String test, String original) {
+        char[] testChars = test.toLowerCase().toCharArray();
+        char[] originalChars = original.toLowerCase().toCharArray();
+        Arrays.sort(testChars);
+        Arrays.sort(originalChars);
+        return Arrays.equals(testChars, originalChars);
+    }
+}
