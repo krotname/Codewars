c4ef26f0089a2b2e1416b85eac503c904708e413
Овчаренко Андрей Александрович <AAleksOvcharenko@sberbank.ru>
2022-02-27 22:15:51 +0300

    Create FindMissingLetter.java

---
+++ src/main/java/kyu6/FindMissingLetter.java
@@ -0,0 +1,35 @@
+package kyu6;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindMissingLetter {
+
+    private static ArrayList<Character> alphabet = new ArrayList<>();
+
+    {
+        for (char a = 'a'; a <= 'z'; a++) {
+            alphabet.add(a);
+        }
+    }
+
+    public static char findMissingLetter(char[] array) {
+
+        int indexStart = alphabet.indexOf(Character.toLowerCase(array[0]));
+        for (int i = 0; i < array.length; i++) {
+            if (!alphabet.get(i + indexStart).equals(Character.toLowerCase(array[i])))
+                return array[0] > 96 ? alphabet.get(i + indexStart) : Character.toUpperCase(alphabet.get(i + indexStart));
+        }
+        return ' ';
+    }
+
+    @Test
+    public void exampleTests() {
+        assertEquals('e', FindMissingLetter.findMissingLetter(new char[]{'a', 'b', 'c', 'd', 'f'}));
+        assertEquals('P', FindMissingLetter.findMissingLetter(new char[]{'O', 'Q', 'R', 'S'}));
+    }
+
+}
