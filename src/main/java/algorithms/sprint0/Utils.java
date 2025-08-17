ac301e61a61b38f1714623bf003330da213bb787
Andrei.Ovcharenko <Andrei.Ovcharenko@lomt.ru>
2025-08-18 00:30:34 +0300

    Utils

--- src/main/java/algorithms/sprint0/Utils.java
+++ src/main/java/algorithms/sprint0/Utils.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Utils {
 
     public static List<Integer> readList(BufferedReader reader) throws IOException {
